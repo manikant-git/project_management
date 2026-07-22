ee68da00a5e599b00682f8af382362c332dbdb36
Kavulapalli Manikanta <manikant14568@gmail.com>
2026-07-22 16:22:58 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,1 +1,11 @@
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-0aba19e56f3eaec05"
+}
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
