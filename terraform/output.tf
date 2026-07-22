83f76ad0894861e3272a9fdfef58e06b999422bc
Kavulapalli Manikanta <manikant14568@gmail.com>
2026-07-22 17:02:39 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,24 @@
 output "instance_id" {
-  value = module.web_server.instance_id
+  value = backend-server.instance_id
 }
 
 output "public_ip" {
-  value = module.web_server.public_ip
+  value = backend-server.public_ip
 }
 
 output "private_ip" {
-  value = module.web_server.private_ip
+  value = backend-server.private_ip
+}
+
+
+output "instance_id" {
+  value = frontend-server.instance_id
+}
+
+output "public_ip" {
+  value = frontend-server.public_ip
+}
+
+output "private_ip" {
+  value = frontend-server.private_ip
 }
